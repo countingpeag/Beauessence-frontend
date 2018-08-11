4d656e757ca46c7621ed72929ed23ebc5e612b2d
Erick Palma <omar4335@hotmail.com>
2018-08-11 18:38:50 -0500

    Profile

--- src/components/AppContent/Profile/index.js
+++ src/components/AppContent/Profile/index.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import {Grid, Row, Col, Image, Label, OverlayTrigger, Popover}  from 'react-bootstrap';
+import {storage} from '../../Storage/Storage';
+import '../../../styles/ProfileStyles.css';
 
 class Profile extends Component{
+
     render(){
+        const {firstName, lastName, userName } = storage.getContent();
+
+        const popoverRight = (
+            <Popover id="popover-positioned-right">
+              <strong>Hola!</strong> Bienvenido {storage.getContent().firstName}.
+            </Popover>
+        );
+
         return(
-            <div>Profile</div>
+            <Grid>
+                <Row>
+                    <Col xs={5} sm={5} md={5} className="info">
+                        <OverlayTrigger trigger="click" placement="right" overlay={popoverRight}>
+                            <Image className="avatar" src={require("../../../images/logo.jpg")} rounded />
+                        </OverlayTrigger >
+                        <h1><Label className="h1Profile">Usuario: {userName}</Label></h1>
+                        <h1><Label className="h1Profile">Nombre: {firstName}</Label> </h1>
+                        <h1><Label className="h1Profile">Apellido: {lastName}</Label></h1>
+                    </Col>
+                </Row>
+            </Grid>
         );
     }
 }

541fb64e1c8543f74590e9c13ea65d1d9b4ca199
Erick Palma <omar4335@hotmail.com>
2018-08-12 15:39:16 -0500

    BodyHome (table)

---
+++ src/components/AppContent/Home/BodyHome.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {Table} from 'react-bootstrap';
+
+class BodyHome extends Component{
+
+    render(){
+        return(
+            <Table striped bordered condensed hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Servicio</th>
+                        <th>Detalles</th>
+                        <th>Comentarios</th>
+                        <th>Fecha</th>
+                        <th>Precio</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td colSpan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </Table>
+        );
+    }
+}
+
+export default BodyHome;
--- src/components/AppContent/Home/index.js
+++ src/components/AppContent/Home/index.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import {Grid, Row, Col} from 'react-bootstrap';
 import HeaderHome from './HeaderHome/HeaderHome';
+import BodyHome from './BodyHome';
 
 class Home extends Component{
     render(){
         return(
-            <HeaderHome />
+            <Grid>
+                <Row>
+                    <Col>
+                        <HeaderHome />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <BodyHome />
+                    </Col>
+                </Row>
+            </Grid>
         );
     }
 }
